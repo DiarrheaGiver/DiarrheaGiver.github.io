85cabae03262b9d47313d7cae1c38b214af3e120
DiarrheaGiver <163147510+DiarrheaGiver@users.noreply.github.com>
2024-11-24 17:51:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+let QuestionList = [
+    {
+        Text: "Социальный ...",
+        Correct: ["Пранк"],
+        Wrong: ["Проект", "Лифт", "Туалет"],
+    },
+    {
+        Text: "Я хочу ...",
+        Correct: ["Отдыхать"],
+        Wrong: ["Работать", "Делать лабы", "Ничего"],
+    },
+]
+
+function getRandomInt(max) {
+    let a = Math.floor(Math.random() * max);
+    return a;
+}
+
+const TestList = document.getElementById("TestList");
+
+function FinishTest(){
+    let FullCorrect = 0;
+    let Correct = 0;
+    let List = TestList.children;
+    for(Question of List){
+        let NumOfCorrectAnswers = 0;
+        let NumOfWrongAnswers = 0;
+        let CorrectAnswers = Question.getElementsByClassName("CorrectAnswer");
+        let WrongAnswers = Question.getElementsByClassName("WrongAnswer");
+        let CAN = CorrectAnswers.length;
+        let WAN = WrongAnswers.length;
+        for(Answer of CorrectAnswers){
+            Answer.nextElementSibling.style.color = "#00ff00"
+            if(Answer.checked){
+                NumOfCorrectAnswers++;
+            }
+        }
+        for(Answer of WrongAnswers){
+            Answer.nextElementSibling.style.color = "#ff0000"
+            if(Answer.checked){
+                NumOfWrongAnswers++;
+            }
+        }
+        if(NumOfCorrectAnswers == CAN && NumOfWrongAnswers == 0){
+            FullCorrect++;
+        }
+    }
+
+    document.getElementById("FinishTest").remove();
+
+    document.getElementById("TestFinisher").insertAdjacentHTML("beforeend", `<p>Ваш результат: ${FullCorrect}/${List.length}</p>`)
+}
+
+document.getElementById("FinishTest").onclick = FinishTest
+
+let TempQuestionList = [...QuestionList];
+
+while (TempQuestionList.length > 0) {
+    let i = getRandomInt(TempQuestionList.length);
+    let Question = TempQuestionList[i];
+    let Div = document.createElement("div");
+    Div.classList.add("QuestionContainer");
+    Div.innerHTML = `<p">${Question.Text}</p>`;
+
+    let Variants = [...Question.Correct, ...Question.Wrong]
+    let Line = Question.Correct.length;
+    while (Variants.length > 0) {
+        let j = getRandomInt(Variants.length);
+        let item = Variants[j];
+        let Type = j < Line ? "CorrectAnswer" : "WrongAnswer"
+        Div.insertAdjacentHTML("beforeend", `<div>
+    <input type="checkbox" name="${item}" class="${Type}"/>
+    <label>${item}</label>
+  </div>`);
+
+        if (Type == "CorrectAnswer") {
+            Line--;
+        }
+        Variants.splice(j, 1);
+    }
+
+    TestList.append(Div);
+    TempQuestionList.splice(i, 1);
+}
